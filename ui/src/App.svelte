3c4349adcb15f248401ab64542bf10c113af9259
Alex <alex@emery.xyz>
2022-08-09 21:31:57 +0100

    Added users to UI
    basic login/logout functionality
    token is added into url shortening request.
    this is just for testing.

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,17 +1,46 @@
 <script>
-	import Router from 'svelte-spa-router'
+	import Router from "svelte-spa-router";
 	import Home from "./routes/Home.svelte";
 	import About from "./routes/About.svelte";
+
+	import { isAuthenticated, user, token } from "./store";
+	import auth from "./authService";
+	import { onMount } from "svelte";
+
 	const routes = {
 		// Exact path
 		"/": Home,
 		"/about": About,
 	};
+
+	let auth0Client
+	onMount(async () => {
+		auth0Client = await auth.createClient();
+
+		isAuthenticated.set(await auth0Client.isAuthenticated());
+		user.set(await auth0Client.getUser());
+	});
+
+	async function login() {
+		await auth.loginWithPopup(auth0Client);
+		token.set(await auth0Client.getTokenSilently())
+	}
+	function logout() {
+		return auth0Client.logout({
+			returnTo: window.location.hostname 
+		});
+	}
+
 </script>
 
 <main>
 	<h1>Yock</h1>
 	<Router {routes} />
+	{#if $isAuthenticated}
+	<button on:click={logout}>Logout</button>
+	{:else}
+	<button on:click={login}>Login</button>
+	{/if}
 </main>
 
 <style>
--- ui/src/routes/home.svelte
+++ ui/src/routes/home.svelte
@@ -1,61 +1,72 @@
 <script>
-  import {link} from 'svelte-spa-router'
-	let newUrl = "";
-	function validURL(str) {
-		var pattern = new RegExp(
-			"^(https?:\\/\\/)?" + // protocol
-				"((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-				"((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-				"(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-				"(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-				"(\\#[-a-z\\d_]*)?$",
-			"i"
-		); // fragment locator
-		return !!pattern.test(str);
-	}
+    import { link } from "svelte-spa-router";
+    import { token } from "../store";
 
-	async function submit(e) {
-		const formData = new FormData(e.target);
-		const data = {};
-		for (let field of formData) {
-			const [key, value] = field;
-			data[key] = value;
+    let tokenValue;
+    token.subscribe((value) => {
+		console.log(value)
+        tokenValue = value;
+    });
+    let newUrl = "";
+    function validURL(str) {
+        var pattern = new RegExp(
+            "^(https?:\\/\\/)?" + // protocol
+                "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+                "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+                "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+                "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+                "(\\#[-a-z\\d_]*)?$",
+            "i"
+        ); // fragment locator
+        return !!pattern.test(str);
+    }
+
+    async function submit(e) {
+        const formData = new FormData(e.target);
+        const data = {};
+        for (let field of formData) {
+            const [key, value] = field;
+            data[key] = value;
+        }
+
+        if (validURL(data.url)) {
+            newUrl = await shortenURL(data.url);
+        } else {
+            newUrl = "please enter a valid url";
+        }
+    }
+
+    const shortenURL = async (url) => {
+        let headers = {
+            "content-type": "application/json",
+        };
+		if (tokenValue !== undefined) {
+			headers["authorization"] = "Bearer " + tokenValue
 		}
 
-		if (validURL(data.url)) {
-			newUrl = await shortenURL(data.url);
-		} else {
-			newUrl = "please enter a valid url";
-		}
-	}
+        const response = await fetch("/api/shorten", {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify({
+                url: url,
+            }),
+        });
 
-	const shortenURL = async (url) => {
-		const response = await fetch("/api/shorten", {
-			method: "POST",
-			headers: {
-				"content-type": "application/json"
-			},
-			body: JSON.stringify({
-				url: url,
-			}),
-		});
-
-		const data = await response.json();
-		return data.url;
-	};
+        const data = await response.json();
+        return data.url;
+    };
 </script>
 
 <main>
-	<p>Enter a url to shorten below</p>
-	<form on:submit|preventDefault={submit}>
-		<input type="text" id="url" name="url" />
-		<button type="submit"> Shorten! </button>
-	</form>
+    <p>Enter a url to shorten below</p>
+    <form on:submit|preventDefault={submit}>
+        <input type="text" id="url" name="url" />
+        <button type="submit"> Shorten! </button>
+    </form>
 
-	{#if newUrl != ""}
-		<div>{newUrl}</div>
-	{/if}
+    {#if newUrl != ""}
+        <div>{newUrl}</div>
+    {/if}
 
-  <a href="/about" use:link>About</a>
+    <a href="/about" use:link>About</a>
 </main>
-
